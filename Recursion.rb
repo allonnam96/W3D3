f633d8446d279076814b79fd74bad4a311b9b87a
Francis Cawog <Franciscawog@gmail.com>
2023-09-06 15:14:53 -0400

    bsearch

--- Recursion.rb
+++ Recursion.rb
@@ -28,19 +28,7 @@ def deep_dup(array)
         new_array << deep_dup(ele)
     end
    new_array
-
 end
-
-
-# robot_parts = [
-#   ["nuts", "bolts", "washers"],
-#   ["capacitors", "resistors", "inductors"]
-# ]
-# p deep_dup(robot_parts[1])
-# robot_parts_copy = robot_parts.dup
-# robot_parts_copy[1] << "LEDs"
-# p deep_dup(robot_parts[1])
-
 
 def fibonacci(n)
     return [] if n == 0
@@ -49,4 +37,26 @@ def fibonacci(n)
 
     last_fibo = fibonacci(n-1)
     last_fibo << last_fibo[-1] + last_fibo[-2]
+end
+
+# def bsearch(array, target)
+#     return nil if array.empty?
+#     return array.length - 1 if array[-1] == target  
+#     bsearch(array[0...-1], target)
+# end
+
+def bsearch(array, target)
+    middle = array.length / 2
+    return middle if array[middle] == target
+    return nil if array.empty?
+
+    if array[middle] > target
+        return bsearch(array[0...middle], target)
+    else
+        if bsearch(array[(middle+1)..-1], target) != nil
+            return middle + 1 + bsearch(array[(middle+1)..-1], target)
+        else 
+            return nil
+        end
+    end
 end
